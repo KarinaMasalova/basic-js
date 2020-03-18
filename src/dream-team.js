2df391c8b216cdfc119114e38c9471ebe2393761
Karina Masalova <masalova.karina@mail.ru>
2020-03-19 01:43:35 +0300

    feat: add dream-team solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,12 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if(!members) return false;
+
+  let array = [];
+
+  for(let i = 0; i < members.length; i++) {
+    if(typeof members[i] === 'string') {
+      array.push(members[i].trim().charAt(0).toUpperCase());
+    }
+  }
+  return array.sort().join('');
 };
