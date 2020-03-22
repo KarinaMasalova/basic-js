79cb11a49802da3ee63a70603b25964c16c3a3c3
Karina Masalova <masalova.karina@mail.ru>
2020-03-22 14:03:20 +0300

    feat: add transform-array solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,19 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+
+    if(!Array.isArray(arr)) throw new Error;
+
+    let controls = ['--discard-next', '--discard-prev', '--double-next', '--double-prev'];
+   
+    let newArr = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if ( !controls.includes(arr[i]) ) newArr.push(arr[i]);
+
+        if ( arr[i] == controls[0] && arr[i + 1] !== undefined) i++;
+        if ( arr[i] == controls[1]) newArr.pop();
+        if ( arr[i] == controls[2] && arr[i + 1] !== undefined) newArr.push(arr[i + 1]);
+        if ( arr[i] == controls[3] && arr[i - 1] !== undefined) newArr.push(arr[i - 1]);
+    }
+
+    return newArr;
 };
